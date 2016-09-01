3c6808352f42319505a5b998ba28d827469cad76
Jonathan Cisneros <jonathan.cisneros@southworks.com>
2016-09-01 17:52:57 -0300

    testing

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,5 @@
-(function ($) {
-    $(document).ready(function() {
-        $('#container').packery();
-
-        if (window.interactions) {
-            window.interactions.setup();
-        }
-
-
-        $(".search-filter-button").on("click", function () {
-            var navTrigger = $("#nav-trigger");
-            navTrigger.prop("checked", !navTrigger.prop("checked"));
-            return false;
-        });
-    });
-}(jQuery));
+$(".search-filter-button").on("click", function () {
+    var navTrigger = $("#nav-trigger");
+    navTrigger.prop("checked", !navTrigger.prop("checked"));
+    return false;
+});
